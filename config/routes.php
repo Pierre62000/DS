01f6b383d13f57df111309af117caff776bc9491
Pierre62000 <pierre.ritzzo@hotmail.com>
2021-02-17 15:07:00 +0000

    Page accueil Q1 complétée

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,21 @@
 <?php
     
 function getPage(){
+
     $lesPages['accueil'] = "accueilControleur";
-    $contenu = $lesPages['accueil'];
-    return $contenu; 
+
+    if (isset($_GET['page'])){
+        $page = $_GET['page'];
+    } else{
+        $page = 'accueil';
+    }
+
+    if (isset($lesPages[$page])){
+        $contenu = $lesPages[$page];
+    } else{
+        $contenu = $lesPages['accueil'];
+    }    
+    return $contenu;
 }
+
 ?>
